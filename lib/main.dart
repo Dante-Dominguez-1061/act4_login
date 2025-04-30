ea6e104f73b270c4de29820325f7fbb39f187a44
DANTE_DOMINGUEZ_DEL_HIERRO <a.22308051281061@cbtis128.edu.mx>
2025-04-30 17:32:15 +0000

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,131 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Game Store App',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blue.shade400,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Aquí puedes agregar la lógica para volver a la pantalla anterior
+          },
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade400, Colors.blue.shade200],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/logo.png', width: 250),
+              const Text(
+                'Iniciar sesión',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+
+              // Campo de Email
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email:',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 15.0),
+
+              // Campo de Contraseña
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true, // Para ocultar la contraseña
+              ),
+              const SizedBox(height: 30.0),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aquí puedes agregar la lógica para iniciar sesión
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    textStyle: const TextStyle(fontSize: 18.0),
+                  ),
+                  child: const Text(
+                    'Iniciar sesión',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+
+              // Botón "Registrarse"
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aquí puedes agregar la lógica para registrarse
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    textStyle: const TextStyle(fontSize: 18.0),
+                  ),
+                  child: const Text(
+                    'Registrarse',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+
+              // Texto "¿Olvidaste tu contraseña?"
+              TextButton(
+                onPressed: () {
+                  // Aquí puedes agregar la lógica para recuperar la contraseña
+                },
+                child: const Text(
+                  '¿Olvidaste tu contraseña?',
+                  style: TextStyle(color: Colors.blueAccent),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
